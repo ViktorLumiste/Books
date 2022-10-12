c6da071e2c7736e8b788c889136bda9d5fb98f98
ViktorLumiste <viktor.lumiste@gmail.com>
2022-10-12 10:57:52 +0300

    As a user I can add a book and have it saved to local storage [delivers#183520764]

--- app.js
+++ app.js
@@ -8,30 +8,42 @@ data.addEventListener('click', deleteBook)
 form.addEventListener('submit', addBook)
 
 function addBook(e) {
-    const row = data.insertRow(1);
-    const cell1 = row.insertCell(0);
-    const cell2 = row.insertCell(1);
-    const cell3 = row.insertCell(2);
-    const cell4 = row.insertCell(3);
+    const row = data.insertRow(1)
+    const cell1 = row.insertCell(0)
+    const cell2 = row.insertCell(1)
+    const cell3 = row.insertCell(2)
+    const cell4 = row.insertCell(3)
     const cross = document.createElement('a')
     cross.appendChild(document.createTextNode('X'))
     cross.className = 'blue-text text-darken-2 secondary-content'
     cross.setAttribute('href', '#')
-    cell1.innerHTML = titleIn.value;
-    cell2.innerHTML = nameIn.value;
-    cell3.innerHTML = ISBNIn.value;
-    cell4.appendChild(cross) ;
-    e.preventDefault()
+    cell1.innerHTML = titleIn.value
+    cell2.innerHTML = nameIn.value
+    cell3.innerHTML = ISBNIn.value
+    cell4.appendChild(cross)
+    const toLSData = [titleIn.value, nameIn.value, ISBNIn.value]
+    addBookLS(toLSData)
     titleIn.value = ""
     nameIn.value = ""
     ISBNIn.value = ""
+    e.preventDefault()
 }
 function deleteBook(e){
-    if(e.target.textContent === "X"){
-        if(confirm( "are you sure you want to remove this book?")){
+    let curBook
+    if (e.target.textContent === "X") {
+        if (confirm("are you sure you want to remove this book?")) {
             curBook = e.target.parentElement.parentElement.rowIndex
             data.deleteRow(curBook)
         }
     }
 }
-
+function addBookLS(book){
+    let books
+    if (localStorage.getItem("books") === null){
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem("books"))
+    }
+    books.push(book)
+    localStorage.setItem("books", JSON.stringify(books))
+}
