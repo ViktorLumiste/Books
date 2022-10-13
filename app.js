d2f2dbf9cff86c0366a5c67605747f636d43b409
ViktorLumiste <viktor.lumiste@gmail.com>
2022-10-13 10:49:48 +0300

    As a user I can view saved book entries loaded from local storage[delivers#183530841]

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const data = document.querySelector('#bookData')
 
 data.addEventListener('click', deleteBook)
 form.addEventListener('submit', addBook)
+document.addEventListener("DOMContentLoaded", getBooksFromLS)
 
 function addBook(e) {
     const row = data.insertRow(-1)
@@ -70,4 +71,31 @@ function deleteBookLS(book){
         }
     })
     localStorage.setItem('books', JSON.stringify(books))
+}
+function getBooksFromLS(){
+    let books
+    if (localStorage.getItem("books") === null){
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem("books"))
+    }
+    console.log(books)
+    books.forEach((book, bookIndex) => {
+        let title = book[0]
+        let author = book[1]
+        let ISBN = book[2]
+        const row = data.insertRow(bookIndex+1)
+        const cell1 = row.insertCell(0)
+        const cell2 = row.insertCell(1)
+        const cell3 = row.insertCell(2)
+        const cell4 = row.insertCell(3)
+        const cross = document.createElement('a')
+        cross.appendChild(document.createTextNode('X'))
+        cross.className = 'blue-text text-darken-2 secondary-content'
+        cross.setAttribute('href', '#')
+        cell1.innerHTML = title
+        cell2.innerHTML = author
+        cell3.innerHTML = ISBN
+        cell4.appendChild(cross)
+    })
 }
